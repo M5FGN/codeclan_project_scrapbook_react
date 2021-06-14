37a78839785478f469ced7e03df3d48437d81825
Ahmet Giritli <akgiritli@gmail.com>
2021-06-14 14:41:43 +0100

    added password check to signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,34 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import Request from "../helpers/request";
 import { useHistory } from "react-router-dom";
 import Logo from "./Logo"
 import Footer from "../components/Footer";
+import { isCompositeComponentWithType } from "react-dom/test-utils";
 
 const SignUp = () => {
+
+  const [error, setError] = useState(null);
+  const [email, setEmail] = useState(null);
+
   const request = new Request("http://localhost:8080/api/users");
+  const emailReq = new Request("http://localhost:8080/api/users/signup");
   let history = useHistory();
 
-  let errorMessage = "";
+
+  // const emailCheck = (event) => {
+  //   setEmail(event.target.value);
+  //   emailReq.getByEmail(email).then((user) => setUserFound(user));
+  //   console.log(userFound)
+  // }  
+
   const handleSignUp = (event) => {
     event.preventDefault();
+    const email = event.target.email.value;
     const name = event.target.name.value;
     const password = event.target.password.value;
     const confirmPassword = event.target.confirmPassword.value;
     const dob = event.target.DOB.value;
 
+    const foundUser = emailReq.getByEmail(email);
+
     const user = {
+      email: email,
       name: name,
       password: password,
       dateOfBirth: dob,
     };
 
-    if (password === confirmPassword) {
+    if (password === confirmPassword && !user) {
       request.post(user);
       history.push("/login");
     } else {
-      errorMessage = "Passwords dont't match";
+      setError("password's don't match")
+      console.log("error message")
     }
   };
+
   return (
 
     <div>
@@ -38,6 +56,10 @@ const SignUp = () => {
     <div className="card formbox">
       <h2>Sign Up to ScrapBook ...</h2>
       <form className="form" onSubmit={handleSignUp}>
+        <label htmlFor="">Email:</label>
+        <br></br>
+        <input type="text" name="email"/>
+        <br></br>
         <label htmlFor="">Name:</label>
         <br></br>
         <input type="text" name="name" />
@@ -56,7 +78,7 @@ const SignUp = () => {
         <br></br>
         <input className="button" type="submit" value="Sign Up..." />
       </form>
-      <p>{errorMessage}</p>
+      <p>{error}</p>
     </div>
     <Footer />
     </div>

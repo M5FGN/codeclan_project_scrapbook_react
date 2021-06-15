7334a5791df1bb64f1d98abfa69acfa840b290cf
pooja maheshwari <pooja.chugh15@gmail.com>
2021-06-15 17:31:02 +0100

    done chat file testing

--- src/tests/Chat.test.js
+++ src/tests/Chat.test.js
@@ -4,21 +4,42 @@ import Stomp from "stompjs";
 import Chat from '../components/Chat';
 
 describe("Chat", () => {
+
+    const stubbedFoundRoom = {
+        id:1,
+        roomName:"memories",
+    }
+
+    const stubbedUser = {
+        name: "Ahmet",
+    }
     
     // const mockChangeValue = jest.fn();
     // const stubbedUser = {message: "Hi how are you?", name: "Ahmet"};
     let container;
 
     beforeEach(() => {
-        const messageList = [{sender:"Ahmet", message:"Liam"}]
-        container = render(<Chat messageList={messageList} />);
-      
+         const messageList = [{sender:"Ahmet", message:"Liam"}]
+         container = render(<Chat foundRoom={stubbedFoundRoom} user={stubbedUser}/>);  
     })
+    
     // first test case to check message field is empty
-    xit("shows message field with empty value", () => {
-        const { getByTestId } = render(<Chat  messageList={messageList} />);
-        expect(getByTestId('message').value).toBe("");
-      
+    it("shows message field with empty value", () => {
+        const { getByTestId } = container;
+        //getByTestId("dmessage").value = "testing";
+        expect(getByTestId("dmessage").value).toBe("");   
+      });
+
+    //Second test case to enter message into the field and submit
+    it("enter message into the field", () => {
+        const { getByTestId } = container;
+        getByTestId("dmessage").value = "testing message";
+        getByTestId("submit").onsubmit;  
+
+        const messageList = [{sender:"Ahmet", message:"testing message"}];
+
+        console.log("textcontent == "+getByTestId('dmessagelist').textContent);
+        expect(container.getByTestId('dmessagelist')).toContainHTML("<ul data-testid=\"dmessagelist\" />");
       });
 
 })
